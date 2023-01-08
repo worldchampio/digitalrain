08f08a723ca902bd16ab43006343ae38b72f7484
Magnus Nilsen <magnusnilsenrandaberg@hotmail.com>
2023-01-08 13:48:01 +0100

    asf

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "DigitalRain.h"
 #include <ncurses.h>
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
@@ -8,22 +9,11 @@ int main(int argc, char* argv[])
     curs_set(0);
     start_color();
     int color{COLOR_GREEN}; 
-    if(argc == 2){
-        try
-        {
-            color = std::atoi(argv[1]);
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-            color = COLOR_GREEN;
-        }
-        
-    }
-    init_pair(1,color,COLOR_BLACK);
+    if(argc == 2)
+        color = std::atoi(argv[1]);
+    init_pair(1,color >= 0 ? color : COLOR_GREEN,COLOR_BLACK);
 
     timeout(20);
-
     attron(COLOR_PAIR(1));
     DigitalRain();
     return endwin();
