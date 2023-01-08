d33abeaa729308e7c900fbfbe168a580e774c0c3
Magnus Nilsen <magnusnilsenrandaberg@hotmail.com>
2023-01-08 12:36:49 +0100

    add option for color as cmdline argument with error handling

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,20 @@ int main(int argc, char* argv[])
     initscr();
     curs_set(0);
     start_color();
-    init_pair(1,COLOR_GREEN,COLOR_BLACK);
+    int color{COLOR_GREEN}; 
+    if(argc == 2){
+        try
+        {
+            color = std::atoi(argv[1]);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+            color = COLOR_GREEN;
+        }
+        
+    }
+    init_pair(1,color,COLOR_BLACK);
 
     timeout(20);
 
